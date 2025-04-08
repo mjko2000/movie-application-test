a6622cd4da59e26dfef7cdfc02b5cc37642773d1
mjko2000 <ndeva3199@gmail.com>
2025-04-08 17:25:38 +0700

    alias import

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+        },
+      },
+    ],
     'react-native-reanimated/plugin',
   ],
 };
